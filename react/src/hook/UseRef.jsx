b1e2920ea0980020da665c6ecb7389822c53e8b4
everyone <everywangBUG@gmail.com>
2026-01-28 18:04:41 +0800

    useRef配合useState强制渲染视图

--- react/src/hook/UseRef.jsx
+++ react/src/hook/UseRef.jsx
@@ -1,13 +1,23 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const UseRef = () => {
     const inputRef = useRef(null);
+    const numRef = useRef(0);
+    const [, forceRender] = useState(0);
 
     useEffect(() => {
         inputRef.current && inputRef.current.focus();
     })
 
-    return <input ref={inputRef} type='text' />
+    return (<div>
+        <input ref={inputRef} type='text' />
+        <div
+            onClick={() => {
+                numRef.current += 1;
+                forceRender(Math.random());
+            }}
+        >{numRef.current}</div>
+    </div>)
 }
 
 export default UseRef;
