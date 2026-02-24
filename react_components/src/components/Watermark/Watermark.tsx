d608142aab45790cb94ebc3c835e57d97a773078
everyone <everywangBUG@gmail.com>
2026-02-24 20:21:28 +0800

    Watermark1

---
+++ react_components/src/components/Watermark/Watermark.tsx
@@ -0,0 +1,43 @@
+import React, { CSSProperties, ReactNode, useRef } from 'react';
+
+interface WatermarkProps {
+    style?: CSSProperties;
+    className?: string;
+    width?: number;
+    height?: number;
+    rotate?: number;
+    zIndex?: number | string;
+    image?: string;
+    content?: string | string[];
+    fontStyle?: {
+        color?: string;
+        fontFamily?: string;
+        fontSize?: number | string;
+        fontWeight?: number | string;
+    };
+    gap?: [number, number];
+    offset?: [number, number];
+    getContainer?: () => HTMLElement;
+    children?: ReactNode
+}
+
+const Watermark: React.FC<WatermarkProps> = (props) => {
+    const {
+        content,
+        style,
+        className
+    } = props;
+
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    return props.children ?
+        (<div
+            style={style}
+            className={className}
+            ref={containerRef}
+        >
+            {props.children}
+        </div>) : null
+}
+
+export default Watermark;
