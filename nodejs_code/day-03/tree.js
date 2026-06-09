0bf061d791741495201d40849ed84507e5c01658
everyone <everywangBUG@gmail.com>
2026-06-09 22:12:40 +0800

    tree.js

--- nodejs_code/day-03/tree.js
+++ nodejs_code/day-03/tree.js
@@ -2,21 +2,41 @@
 const fs = require("node:fs/promises");
 const path = require("node:path");
 
-async function generateTree(dir = process.cwd()) {
-  let res = "# 输出:";
+async function generateTree(dir = process.cwd(), prefix = "") {
+  let res = "";
+  let directories = 0;
+  let files = 0;
+  if (!prefix) {
+    res += `# ${path.basename(dir)}\n`;
+  }
   // 递归遍历path目录下的文件和文件夹
   const entries = await fs.readdir(dir, { withFileTypes: true });
-  // console.log(entries, "entries");
-  for (let entry of entries) {
-    // console.log(entry);
-    if (entry.isFile()) {
-      res += `# ├── ${entry.name}`;
-      res += "\n";
-    } else if (entry.isDirectory()) {
-      const fullPath = path.join(dir, entry.name);
-      await generateTree(fullPath);
-      res += "# ├──";
+  // const sortByCharEntries = entries.sort((a, b) => a.name.localeCompare(b));
+  for (let i = 0; i < entries.length; i++) {
+    const isLast = i === entries.length - 1;
+    // console.log(isLast, "isLast888");
+    const line = isLast  ? "└──" : "├──"
+    const entry = entries[i];
+    res += `# ${prefix}${line}${entry.name} \n`;
+    files += 1;
+
+    if (entry.isDirectory()) {
+      console.log(entry.name === "node_modules");
+      if (entry.name !== "node_modules" || entry.name !== ".git") {
+        directories += 1;
+        const fullPath = path.join(dir, entry.name);
+        // console.log(isLast, "isLast77");
+        const newPrefix = prefix + (isLast ? "   " : "|  ");
+        // console.log(newPrefix, "newPrefix");
+        const subTree = await generateTree(fullPath, newPrefix);
+        res += subTree;
+      }
     }
+  }
+
+  if (!prefix) {
+    res += "#\n"
+    res += `# ${directories} directories, ${files} files`;
   }
 
   return res;
@@ -24,7 +44,7 @@ async function generateTree(dir = process.cwd()) {
 
 async function main() {
   try {
-    const tree = await generateTree("./src");
+    const tree = await generateTree("../day-03");
     console.log(tree);
   } catch (err) {
     console.error(err);
